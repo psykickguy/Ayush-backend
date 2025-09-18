0b841f22cf9173b5bf5d9efde9ba61c6250be7fb
prat-347-ik <chavanpratik2005@gmail.com>
2025-09-18 19:44:33 +0530

    Updated User Management Page Backend

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
+connectDB();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-// Simple route
-app.get("/", (req, res) => {
-  res.send("Server is running on port 8080 🚀");
-});
+app.use("/api/users", userRoutes);
 
-// Start server
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
